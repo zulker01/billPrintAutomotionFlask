9ac26d44f0e5d18784805568413c06e8c6e13658
Zulker Nayeen <zulker.nayeen@konasl.com>
2026-05-03 12:50:08 +0600

    initial

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, send_file
+from PIL import Image
+import io
+
+app = Flask(__name__)
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    files = request.files.getlist("images")
+
+    W, H = 3508, 2480  # A4 landscape
+    cols, rows = 3, 2
+    margin = 50
+
+    cell_w = (W - margin*(cols+1)) // cols
+    cell_h = (H - margin*(rows+1)) // rows
+
+    canvas = Image.new("RGB", (W, H), "white")
+
+    for i, file in enumerate(files[:6]):
+        img = Image.open(file)
+        img = img.resize((cell_w, cell_h))
+
+        x = margin + (i % cols) * (cell_w + margin)
+        y = margin + (i // cols) * (cell_h + margin)
+
+        canvas.paste(img, (x, y))
+
+    output = io.BytesIO()
+    canvas.save(output, format="PDF")
+    output.seek(0)
+
+    return send_file(output, mimetype="application/pdf")
